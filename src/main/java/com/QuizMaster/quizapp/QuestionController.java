349681fcb3472d4f85d4804cac808d9b6d114227
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-03 17:08:44 +0530

    question controller

--- src/main/java/com/QuizMaster/quizapp/QuestionController.java
+++ src/main/java/com/QuizMaster/quizapp/QuestionController.java
@@ -1,8 +1,16 @@
 package com.QuizMaster.quizapp;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("question")
 public class QuestionController {
+
+    @GetMapping("allQuestions")
+    public String getAllQuestions(){
+
+    }
 }
