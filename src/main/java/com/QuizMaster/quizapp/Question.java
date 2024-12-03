d915067249185f51ed3046f8332a98e99d2afcfb
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-03 23:15:19 +0530

    question class new

--- src/main/java/com/QuizMaster/quizapp/Question.java
+++ src/main/java/com/QuizMaster/quizapp/Question.java
@@ -6,4 +6,7 @@ public class Question {
     private String questionTitle;
     private String option1;
     private String option2;
+    private String option3;
+    private String option4;
+    private String ;
 }
