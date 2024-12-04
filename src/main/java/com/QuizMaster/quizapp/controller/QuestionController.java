6bc8d39d4d01b62963d6a6010e6a87e59981f64e
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-04 22:09:25 +0530

    entity and data

--- src/main/java/com/QuizMaster/quizapp/controller/QuestionController.java
+++ src/main/java/com/QuizMaster/quizapp/controller/QuestionController.java
@@ -1,10 +1,14 @@
 package com.QuizMaster.quizapp.controller;
 
 
+import com.QuizMaster.quizapp.Question;
+import com.QuizMaster.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("question")
@@ -15,7 +19,7 @@ public class QuestionController {
     QuestionService questionService;
 
     @GetMapping("allQuestions")
-    public String getAllQuestions(){
+    public List<Question> getAllQuestions(){
       return questionService.getAllQuestions();
     }
 }
---
+++ src/main/java/com/QuizMaster/quizapp/service/QuestionService.java
@@ -0,0 +1,14 @@
+package com.QuizMaster.quizapp.service;
+
+
+import com.QuizMaster.quizapp.Question;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+    public List<Question> getAllQuestions() {
+
+    }
+}
