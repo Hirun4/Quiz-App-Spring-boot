358c34b01eae500a4054a5c75bcef3e7ce440f3a
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-06 12:03:23 +0530

    quiz controller

--- src/main/java/com/QuizMaster/quizapp/controller/QuizController.java
+++ src/main/java/com/QuizMaster/quizapp/controller/QuizController.java
@@ -1,10 +1,13 @@
 package com.QuizMaster.quizapp.controller;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("quiz")
 public class QuizController {
+
+    public ResponseEntity<String> createQuiz()
 }
