f1de529f58d1f41033a976ca149f284a63731548
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-06 17:44:44 +0530

    quiz service

--- src/main/java/com/QuizMaster/quizapp/service/QuizService.java
+++ src/main/java/com/QuizMaster/quizapp/service/QuizService.java
@@ -1,18 +1,29 @@
 package com.QuizMaster.quizapp.service;
 
 
+import com.QuizMaster.quizapp.dao.QuestionDao;
 import com.QuizMaster.quizapp.dao.QuizDao;
+import com.QuizMaster.quizapp.model.Question;
+import com.QuizMaster.quizapp.model.Quiz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class QuizService {
 
     @Autowired
     QuizDao quizDao;
+    QuestionDao questionDao;
 
     public ResponseEntity<String> createQuiz(String category, int numQ, String title) {
 
+
+        List<Question> questions =
+        Quiz quiz = new Quiz();
+        quiz.setTitle(title);
+        quiz.setQuestions();
     }
 }
