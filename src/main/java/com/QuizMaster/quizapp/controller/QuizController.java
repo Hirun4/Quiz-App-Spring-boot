7959788ecaeee19626fc7751683f8052d072e691
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-06 12:26:02 +0530

    quiz controller

--- src/main/java/com/QuizMaster/quizapp/controller/QuizController.java
+++ src/main/java/com/QuizMaster/quizapp/controller/QuizController.java
@@ -2,6 +2,7 @@ package com.QuizMaster.quizapp.controller;
 
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("quiz")
 public class QuizController {
 
-    public ResponseEntity<String> createQuiz()
+    @PostMapping("create")
+    public ResponseEntity<String> createQuiz(){
+
+    }
 }
