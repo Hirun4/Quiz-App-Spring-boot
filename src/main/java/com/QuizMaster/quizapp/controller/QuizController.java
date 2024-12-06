aaaea8e593e0fd1bc58f79900c2d0738a7a29a49
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-06 12:47:32 +0530

    quiz controller

--- src/main/java/com/QuizMaster/quizapp/controller/QuizController.java
+++ src/main/java/com/QuizMaster/quizapp/controller/QuizController.java
@@ -1,9 +1,11 @@
 package com.QuizMaster.quizapp.controller;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuizController {
 
     @PostMapping("create")
-    public ResponseEntity<String> createQuiz(){
-
+    public ResponseEntity<String> createQuiz(@RequestParam String category,@RequestParam int numQ,@RequestParam String title){
+        return new ResponseEntity<>("Im here", HttpStatus.OK);
     }
 }
