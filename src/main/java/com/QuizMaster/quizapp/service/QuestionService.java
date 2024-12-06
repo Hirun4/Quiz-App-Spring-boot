b9524c3e2e552c3f135e75c72302dd679f225716
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-06 08:16:03 +0530

    add question

--- src/main/java/com/QuizMaster/quizapp/service/QuestionService.java
+++ src/main/java/com/QuizMaster/quizapp/service/QuestionService.java
@@ -14,11 +14,18 @@ public class QuestionService {
     @Autowired
     QuestionDao questionDao;
 
+
+
     public List<Question> getAllQuestions() {
         return questionDao.findAll();
     }
 
     public List<Question> getQuestionsByCategory(String category) {
         return questionDao.findByCategory(category);
+    }
+
+    public String addQuestion(Question question) {
+          questionDao.save(question);
+          return "Success";
     }
 }
