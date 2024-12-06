55e78f23ad88931b95d037f09a1a88dae6da9430
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-06 17:55:37 +0530

    write query

--- src/main/java/com/QuizMaster/quizapp/dao/QuestionDao.java
+++ src/main/java/com/QuizMaster/quizapp/dao/QuestionDao.java
@@ -12,7 +12,7 @@ import java.util.List;
 public interface QuestionDao extends JpaRepository<Question,Integer> {
 
 
-    @Query(value = "",nativeQuery = true)
+    @Query(value = "SELECT * FROM question q Where q.category=:category ORDER BY RANDOM() LIMIT :numQ",nativeQuery = true)
     static List<Question> findRandomQuestionsByCategory(String category, int numQ) {
 
     }
