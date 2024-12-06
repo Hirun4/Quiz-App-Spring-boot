ecd6a3c1419f0895105de060e5d6fb5d07d5b8ec
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-06 17:02:25 +0530

    quiz service

--- src/main/java/com/QuizMaster/quizapp/controller/QuizController.java
+++ src/main/java/com/QuizMaster/quizapp/controller/QuizController.java
@@ -2,6 +2,7 @@ package com.QuizMaster.quizapp.controller;
 
 
 import com.QuizMaster.quizapp.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("quiz")
 public class QuizController {
 
+    @Autowired
     QuizService quizService;
 
     @PostMapping("create")
